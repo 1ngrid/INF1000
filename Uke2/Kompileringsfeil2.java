1a690801fe7a4fefbe87a23114d5ecdef32df899
Ingrid <ingrid.pingvin@gmail.com>
2013-09-03 10:39:38 +0200

    Filer fra seminarromstimen, Uke 2, Gruppe 8 og 9

--- Uke2/Kompileringsfeil2.java
+++ Uke2/Kompileringsfeil2.java
@@ -5,12 +5,10 @@
 class Kompileringsfeil2 {
 
 	int y = 2, z= 3;
-	public static void main(String[] args){
-		/* Kanskje er det en feil her? */
+	/* Kanskje er det en feil her? */
 
-		System.out.println("hva er feil her?");
-		System.out.println(y);
-		// Kommenter på en linje
-		System.out.println(z);
-	}
+	System.out.println("hva er feil her?");
+	System.out.println(y);
+	// Kommenter på en linje
+	System.out.println(z);
 }
